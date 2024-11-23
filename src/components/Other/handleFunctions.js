bdf7a96b25418d3bb4bd4500605b5d50268ab942
Patrick231203 <Patrick.weinkauf@icloud.com>
2024-11-23 15:49:22 +0100

    refactor: added forgotten await statement for db function

--- src/components/Other/handleFunctions.js
+++ src/components/Other/handleFunctions.js
@@ -2,8 +2,8 @@ import log from "../../logger.js";
 import { getByNameDB } from "./db.js";
 
 export async function selectGraph(filename, setGraph, setActiveFiles) {
-  const file = getByNameDB(filename);
-  if (!file) throw new Error(`No file found with the name ${filename}.`);
+  const file = await getByNameDB(filename);
+  if (!file || !file.content) throw new Error(`No file found with the name ${filename}.`);
   setActiveFiles([file]);
 
   const graph = JSON.parse(file.content);
