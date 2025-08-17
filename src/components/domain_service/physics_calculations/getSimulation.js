5cc4e516ebbfecf8c988844cce39b28871e823f5
Patrick231203 <Patrick.weinkauf@icloud.com>
2025-08-17 18:32:17 +0200

    changed init x and y force to gravity force

--- src/components/domain_service/physics_calculations/getSimulation.js
+++ src/components/domain_service/physics_calculations/getSimulation.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import { accuracyBarnesHut, maxDistanceChargeForce, nodeRepulsionMultiplier } from "./physicsGraph.js";
+import { accuracyBarnesHut, gravityForce, maxDistanceChargeForce, nodeRepulsionMultiplier } from "./physicsGraph.js";
 
 export function getSimulation(width, height, linkLength, gravityStrength, nodeRepulsionStrength) {
   const simulation = d3
@@ -19,10 +19,7 @@ export function getSimulation(width, height, linkLength, gravityStrength, nodeRe
         .id((d) => d.id)
         .distance(linkLength)
     )
-    // .forceCenter(width / 2, height / 2).strength(gravityStrength)
-    .force("x", d3.forceX(width / 2).strength(gravityStrength))
-    .force("y", d3.forceY(height / 2).strength(gravityStrength))
-    // .force("gravity", d3.forceCenter(width / 2, height / 2).strength(gravityStrength))
+    .force("gravity", gravityForce(width / 2, height / 2).strength(gravityStrength))
     .alphaMin(0.05);
 
   simulation.randomSource();
