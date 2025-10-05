f536d8e50998c176ec4fe27f26b1087558a8acda
Patrick231203 <Patrick.weinkauf@icloud.com>
2025-10-05 15:48:35 +0200

    cutoff for protein descriptions

--- src/components/domain_service/parsing/uniprotDataParsing.js
+++ src/components/domain_service/parsing/uniprotDataParsing.js
@@ -1,9 +1,28 @@
 export const getDescriptionUniprotData = (jsonData) => {
-  console.log("JSONDAT", jsonData); // NEXT: SHORTEN DESCRIPTION + FIND MORE USEFUL THINGS IN API SUCH AS 'DISEASE'?
   const functionComment = jsonData.comments.find((comment) => comment.commentType === "FUNCTION");
 
   if (functionComment && functionComment.texts && functionComment.texts.length > 0) {
-    return functionComment.texts[0].value;
+    const fullText = functionComment.texts[0].value;
+
+    const MAX_CHARS = 1000;
+
+    if (fullText.length <= MAX_CHARS) {
+      return fullText;
+    }
+
+    const truncatedText = fullText.substring(0, MAX_CHARS);
+
+    const lastPeriodIndex = Math.max(truncatedText.lastIndexOf("."), truncatedText.lastIndexOf("!"), truncatedText.lastIndexOf("?"));
+
+    const ellipsis = "... [Read more on UniProt]";
+
+    if (lastPeriodIndex > 0) {
+      const safeShortText = truncatedText.substring(0, lastPeriodIndex + 1);
+
+      return safeShortText.trim() + ellipsis;
+    }
+
+    return truncatedText.trim() + ellipsis;
   }
 
   return null;
