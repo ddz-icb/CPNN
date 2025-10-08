df289d3ce1e8f2883081322b74ec1d67e7af0cd1
Patrick231203 <Patrick.weinkauf@icloud.com>
2025-10-08 21:55:45 +0200

    also same tooltip for header button + removed deprecated code

--- src/components/gui/reusable_components/headerbarComponents.js
+++ src/components/gui/reusable_components/headerbarComponents.js
@@ -3,12 +3,10 @@ import { Tooltip } from "react-tooltip";
 export function HeaderButton({ icon, onClick, children, tooltip, tooltipId }) {
   return (
     <>
-      <div className="tooltip-wrapper">
-        <a className={"icon-button"} onClick={onClick} data-tooltip-id={tooltipId} data-tooltip-content={tooltip}>
-          {icon}
-        </a>
-        <Tooltip id={tooltipId} place="bottom" effect="solid" className="tooltip" />
-      </div>
+      <a className={"icon-button"} onClick={onClick} data-tooltip-id={tooltipId} data-tooltip-content={tooltip}>
+        {icon}
+      </a>
+      <Tooltip id={tooltipId} place="bottom" effect="solid" className="tooltip" />
       {children}
     </>
   );
