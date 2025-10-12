320163ac305dd26b7ee223080cdb2d04e97a76a4
Patrick231203 <Patrick.weinkauf@icloud.com>
2025-10-12 12:47:17 +0200

    renamed react folder to controllers. react was a stupid idea. whoever thought of that is a dumbass lmao (it was me)

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ import "./styles/buttons.css";
 import { Sidebar } from "./components/adapters/gui/sidebar/sidebar.js";
 import { HeaderBar } from "./components/adapters/gui/headerbar/headerbar.js";
 import { useTheme } from "./components/adapters/state/themeState.js";
-import { RenderControl } from "./components/adapters/react/renderControl.js";
-import { PhysicsControl } from "./components/adapters/react/physicsControl.js";
-import { InitControl } from "./components/adapters/react/initControl.js";
-import { DownloadControl } from "./components/adapters/react/downloadControl.js";
-import { FilterControl } from "./components/adapters/react/filterControl.js";
-import { AppearanceControl } from "./components/adapters/react/appearanceControl.js";
+import { RenderControl } from "./components/adapters/controllers/renderControl.js";
+import { PhysicsControl } from "./components/adapters/controllers/physicsControl.js";
+import { InitControl } from "./components/adapters/controllers/initControl.js";
+import { DownloadControl } from "./components/adapters/controllers/downloadControl.js";
+import { FilterControl } from "./components/adapters/controllers/filterControl.js";
+import { AppearanceControl } from "./components/adapters/controllers/appearanceControl.js";
 import { Error } from "./components/adapters/gui/error/error.js";
-import { useGraphSetup } from "./components/adapters/react/useGraphSetup.js";
+import { useGraphSetup } from "./components/adapters/controllers/useGraphSetup.js";
 
 function App() {
   const { theme } = useTheme();
