94c5d403ac8e240f1f17e72192aae7be2c632c15
Patrick231203 <Patrick.weinkauf@icloud.com>
2026-01-24 14:57:55 +0100

    refactor: prettier search highlights, especially in light mode

--- src/components/adapters/state/themeState.js
+++ src/components/adapters/state/themeState.js
@@ -3,20 +3,21 @@ import { create } from "zustand";
 const indigo = "#073d7fff";
 const lightGrey = "#b2becd";
 const deepNavy = "#0f172a";
-const brightYellow = "ffd400"
+const brightYellow = "rgba(255, 212, 0, 0.75)";
+const coolBlue = "#006aff";
 
 export const lightTheme = {
   name: "light",
   circleBorderColor: indigo,
   textColor: deepNavy,
-  highlightColor: indigo
+  highlightColor: coolBlue,
 };
 
 export const darkTheme = {
   name: "dark",
   circleBorderColor: lightGrey,
   textColor: lightGrey,
-  highlightColor: brightYellow
+  highlightColor: brightYellow,
 };
 
 export const themeInit = lightTheme;
--- src/components/domain/service/canvas_drawing/highlights.js
+++ src/components/domain/service/canvas_drawing/highlights.js
@@ -30,11 +30,11 @@ export function highlightNode(circle, highlightColor) {
 
   const overlay = new PIXI.Graphics();
   overlay.eventMode = "none";
-  overlay.alpha = 0.7;
+  overlay.alpha = 1;
   overlay.blendMode = "add";
-  overlay.circle(0, 0, radius + 4).stroke({ color: highlightColor, width: 3, alpha: 0.8, alignment: 0.5 });
-  overlay.circle(0, 0, radius + 8).stroke({ color: highlightColor, width: 2, alpha: 0.6, alignment: 0.5 });
-  overlay.circle(0, 0, radius + 12).stroke({ color: highlightColor, width: 1, alpha: 0.4, alignment: 0.5 });
+  overlay.circle(0, 0, radius + 4).stroke({ color: highlightColor, width: 3, alpha: 1.0, alignment: 0.5 });
+  overlay.circle(0, 0, radius + 8).stroke({ color: highlightColor, width: 2, alpha: 0.7, alignment: 0.5 });
+  overlay.circle(0, 0, radius + 11).stroke({ color: highlightColor, width: 1, alpha: 0.4, alignment: 0.5 });
 
   const parent = circle.parent;
   if (parent?.addChild) {
