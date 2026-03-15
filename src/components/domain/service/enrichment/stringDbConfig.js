68917dfa44bb07db1164106d276cf383ef41978f
Patrick231203 <Patrick.weinkauf@icloud.com>
2026-03-15 12:14:03 +0100

    refactor: set default_min_confidence to 0.9

--- src/components/domain/service/enrichment/stringDbConfig.js
+++ src/components/domain/service/enrichment/stringDbConfig.js
@@ -2,7 +2,7 @@ export const STRING_DB_BASE_URL = "https://string-db.org/api/json";
 export const STRING_DB_CALLER_ID = "cpnn";
 export const STRING_DB_LINK_ATTRIB = "STRING-DB";
 
-export const DEFAULT_MIN_CONFIDENCE = 0.7;
+export const DEFAULT_MIN_CONFIDENCE = 0.9;
 export const MIN_CONFIDENCE = 0;
 export const MAX_CONFIDENCE = 1;
 
